f92c93ecc974401de17c061a7faf4ba5d0121092
Nelson Liu <nelson.liu@funpodium.net>
2021-12-01 16:53:36 +0800

    feat: add jest config

--- packages/eslint-config-react/lib/.eslintrc.js
+++ packages/eslint-config-react/lib/.eslintrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-    globals: {
-        React: true,
+  globals: {
+    React: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react/jsx-first-prop-new-line': [ 'error', 'multiline' ],
+    'react/jsx-max-props-per-line': [ 'error', { maximum: 1 } ],
+    'react/jsx-curly-spacing': [ 'error', 'never' ],
+    'react/jsx-boolean-value': [ 'error', 'never' ],
+    'react/jsx-one-expression-per-line': [ 'error' ],
+    'react/jsx-tag-spacing': [ 'error', {
+      closingSlash: 'never',
+      beforeClosing: 'never',
+      beforeSelfClosing: 'always',
+    } ],
+    'react/jsx-wrap-multilines': [ 'error', {
+      return: 'parens-new-line',
+      arrow: 'parens-new-line',
+      condition: 'parens-new-line',
+      logical: 'parens-new-line',
+      prop: 'parens',
+    } ],
+    'react/destructuring-assignment': [ 'warn' ],
+    'react/jsx-closing-bracket-location': [ 'error', 'after-props' ],
+    'react/jsx-no-useless-fragment': [ 'error' ],
+    'react/self-closing-comp': [ 'error' ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    extends: [
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-    ],
-    rules: {
-        'react/prop-types': 'off',
-        'react/jsx-first-prop-new-line': [ 'error', 'multiline' ],
-        'react/jsx-max-props-per-line': [ 'error', { maximum: 1 } ],
-        'react/jsx-curly-spacing': [ 'error', 'never' ],
-        'react/jsx-boolean-value': [ 'error', 'never' ],
-        'react/jsx-one-expression-per-line': [ 'error' ],
-        'react/jsx-tag-spacing': [ 'error', {
-            closingSlash: 'never',
-            beforeClosing: 'never',
-            beforeSelfClosing: 'always',
-        } ],
-        'react/jsx-wrap-multilines': [ 'error', {
-            return: 'parens-new-line',
-            arrow: 'parens-new-line',
-            condition: 'parens-new-line',
-            logical: 'parens-new-line',
-            prop: 'parens',
-        } ],
-        'react/destructuring-assignment': [ 'warn' ],
-        'react/jsx-closing-bracket-location': [ 'error', 'after-props' ],
-        'react/jsx-no-useless-fragment': [ 'error' ],
-        'react/self-closing-comp': [ 'error' ],
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
+  },
 }
--- packages/eslint-config/lib/.eslintrc.js
+++ packages/eslint-config/lib/.eslintrc.js
@@ -1,91 +1,104 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "node": true
+  env: {
+    browser: true,
+    node: true,
   },
-  "extends": [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/recommended",
-    "plugin:import/typescript",
-    "standard",
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'standard',
   ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaFeatures": {
-      "jsx": true
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 12,
-    "sourceType": "module"
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  "plugins": [
-    "@typescript-eslint",
+  plugins: [
+    '@typescript-eslint',
+    'import-newlines',
   ],
-  "rules": {
+  rules: {
     // vanilla
-    "indent": [ "error", 2 ],
-    "max-len": [ "error", 120 ],
-    "comma-dangle": [ "error", "always-multiline" ],
-    "arrow-parens": ["error", "as-needed"],
-    "arrow-spacing": [ "error", { "before": true, "after": true }],
-    "array-bracket-spacing": [ "error", "always" ],
-    "space-before-function-paren": [ "error", {
-      "anonymous": "never",
-      "named": "never",
-      "asyncArrow": "always",
+    indent: [ 'error', 2 ],
+    'max-len': [ 'error', 120 ],
+    'comma-dangle': [ 'error', 'always-multiline' ],
+    'arrow-parens': [ 'error', 'as-needed' ],
+    'arrow-spacing': [ 'error', { before: true, after: true } ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'space-before-function-paren': [ 'error', {
+      anonymous: 'never',
+      named: 'never',
+      asyncArrow: 'always',
     } ],
-    "no-use-before-define": "off",
-    "no-unused-vars": "off",
-    "no-console": [ "warn", { "allow": [ "error", "warn", "info" ] } ],
-    "prefer-arrow-callback": [ "error", { "allowNamedFunctions": true } ],
-    "newline-per-chained-call": [ "error", { "ignoreChainWithDepth": 1 } ],
-    "padding-line-between-statements": [ "error",
-      { "blankLine": "always", "prev": "*", "next": "return" },
-      { "blankLine": "always", "prev": "*", "next": "throw" },
-      { "blankLine": "always", "prev": "*", "next": "export" },
-      { "blankLine": "always", "prev": "*", "next": "block-like" },
-      { "blankLine": "always", "prev": "*", "next": "multiline-expression" },
-      { "blankLine": "always", "prev": "singleline-const", "next": "multiline-const" },
-      { "blankLine": "always", "prev": "multiline-const", "next": "multiline-const" },
-      { "blankLine": "never", "prev": "singleline-const", "next": "singleline-const" },
+    'no-use-before-define': 'off',
+    'no-unused-vars': 'off',
+    'no-console': [ 'warn', { allow: [ 'error', 'warn', 'info' ] } ],
+    'function-paren-newline': [ 'error', 'multiline-arguments' ],
+    'function-call-argument-newline': [ 'warn', 'always' ],
+    'prefer-arrow-callback': [ 'error', { allowNamedFunctions: true } ],
+    'newline-per-chained-call': [ 'error', { ignoreChainWithDepth: 1 } ],
+    'padding-line-between-statements': [ 'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: 'throw' },
+      { blankLine: 'always', prev: '*', next: 'export' },
+      { blankLine: 'always', prev: '*', next: 'block-like' },
+      { blankLine: 'always', prev: '*', next: 'multiline-expression' },
+      { blankLine: 'always', prev: 'singleline-const', next: 'multiline-const' },
+      { blankLine: 'always', prev: 'multiline-const', next: 'multiline-const' },
+      { blankLine: 'never', prev: 'singleline-const', next: 'singleline-const' },
     ],
 
     // typescript
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/consistent-type-imports": [ "error" ],
-    "@typescript-eslint/type-annotation-spacing": [ "error", {
-      "before": false,
-      "after": true,
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/consistent-type-imports': [ 'error' ],
+    '@typescript-eslint/type-annotation-spacing': [ 'error', {
+      before: false,
+      after: true,
     } ],
-    "@typescript-eslint/member-delimiter-style": [ "error", {
-      "multiline": {
-        "delimiter": "none",
-        "requireLast": true,
+    '@typescript-eslint/member-delimiter-style': [ 'error', {
+      multiline: {
+        delimiter: 'none',
+        requireLast: true,
       },
-      "singleline": {
-        "delimiter": "semi",
-        "requireLast": false,
+      singleline: {
+        delimiter: 'semi',
+        requireLast: false,
       },
-      "multilineDetection": "brackets",
+      multilineDetection: 'brackets',
     } ],
 
     // import
-    "import/order": "warn",
-    "import/default": "off",
-    "import/newline-after-import": [ "error", { "count": 1 } ],
-    "import/no-named-as-default": "off",
+    'import/order': 'warn',
+    'import/default': 'off',
+    'import/newline-after-import': [ 'error', { count: 1 } ],
+    'import/no-named-as-default': 'off',
+
+    // import-newlines
+    'import-newlines/enforce': [
+      'error',
+      {
+        items: 3,
+        'max-len': 120,
+        semi: false,
+      },
+    ],
   },
-  "settings": {
-    "import/parsers": {
-      "@typescript-eslint/parser": [ ".ts", ".tsx" ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': [ '.ts', '.tsx' ],
     },
-    "import/resolver": {
-      "typescript": {
-        "alwaysTryTypes": true,
-        "project": ".",
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: '.',
       },
     },
-    "node": {
-      "extensions": [ ".js", ".jsx", ".ts", ".tsx" ],
+    node: {
+      extensions: [ '.js', '.jsx', '.ts', '.tsx' ],
     },
-  }
+  },
 }
